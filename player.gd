25d6da18b88e79a439767421f9b18b7f38a9b600
Person317 <gemiyah317@gmail.com>
2024-09-19 10:32:20 -0400

    Switch through animations

--- player.gd
+++ player.gd
@@ -3,8 +3,14 @@ extends CharacterBody2D
 const SPEED = 140.0
 const JUMP_VELOCITY = -200.0
 
+@onready var animated_sprite : AnimatedSprite2D = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var animation_locked : bool = false
+var direction : Vector2 = Vector2.ZERO
+
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -13,15 +19,25 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-	
+	# Normal jump from floor
+		velocity.y = JUMP_VELOCITY
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction:velocity.x = direction.x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		
 	move_and_slide()
+	update_animation()
+	
+func update_animation():
+	if not animation_locked:
+		if direction.x != 0:
+			animated_sprite.play("Run")
+		else:
+			animated_sprite.play("Idle")
+		
+
